88921fd7e2ef7a1abf968a1bf60c657474affd16
Juan Felipe Camargo <juan9505@gmail.com>
2017-11-19 21:51:42 -0500

    Correcciones revisores

--- peticiones/usuarios/getrevisores.php
+++ peticiones/usuarios/getrevisores.php
@@ -1,14 +1,29 @@
 <?php
+function utf8_encode_deep(&$input)
+{
+    if (is_string($input)) {
+        $input = utf8_encode($input);
+    } else if (is_array($input)) {
+        foreach ($input as &$value) {
+            utf8_encode_deep($value);
+        }
+
+        unset($value);
+    } else if (is_object($input)) {
+        $vars = array_keys(get_object_vars($input));
+
+        foreach ($vars as $var) {
+            utf8_encode_deep($input->$var);
+        }
+    }
+}
+
+
 require_once("../config.php");
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 $request = ( array )$request;
-$arr = array(
-    'asignados' => [],
-    'no_asignados' => []
-);
-
     //Reciba variable id del articulo
     //$id_articulo = isset($request["id_articulo"]);
     $id_articulo = 5;
@@ -21,7 +36,7 @@ $resultado = $conex->query($sql);
 
 if ($resultado->num_rows > 0) {
     while ($row = $resultado->fetch_assoc()) {
-        array_push($arr['asignados'], $row);
+        $arr['asignados'][] = array_map("utf8_encode", $row);
     }
 }
 
@@ -30,14 +45,13 @@ $resultado = $conex->query($sql);
 
 if ($resultado->num_rows > 0) {
     while ($row = $resultado->fetch_assoc()) {
-        array_push($arr['no_asignados'], $row);
+        $arr['no_asignados'][] = array_map("utf8_encode", $row);
     }
 }
 
-print_r($arr);
-$close = mysqli_close($conex);
-
 header('Content-type: application/json; charset=utf-8');
     //La respuesta se manda como un json
-echo json_encode($arr, JSON_FORCE_OBJECT);
+echo json_encode($arr);
+
+$close = mysqli_close($conex);
 ?>

5a0e7b195e275ce99a5359471480364b21fdbffb
Juan Felipe Camargo <juan9505@gmail.com>
2017-11-21 09:06:58 -0500

    Revision.php

--- peticiones/articulos/revision.php
+++ peticiones/articulos/revision.php
@@ -1,53 +1,56 @@
 <?php
-
-    date_default_timezone_set('America/Bogota');
-    require_once("../config.php");
-    require_once("../login/funciones.php");
-    session_start();
-    $idUser = $_SESSION['id_usuario'];  
-
-
-    $location = "../../articulos/";
-    $tmp_file_a = $_FILES['file_a']['tmp_name'];
-    $revisado = date("d-m-y-H-i-s") ."-". normalizar($_FILES['file_a']['name']);
+date_default_timezone_set('America/Bogota');
+require_once("../config.php");
+require_once("../login/funciones.php");
+session_start();
+$nickname = $_SESSION['nickname'];
 
 
-    $tmp_file_b = $_FILES['file_b']['tmp_name'];
-    $formulario =  date("d-m-y-H-i-s") ."-". normalizar($_FILES['file_b']['name']);
-
-    $date = date("Y:m:d H:i:s");
-
-    $est_aceptado = $_POST["aprobado"];
-    $id_art = $_POST["id_art"];
+$location = "../../articulos/";
+$tmp_file_a = $_FILES['file_a']['tmp_name'];
+$revisado = date("d-m-y-H-i-s") . "-" . normalizar($_FILES['file_a']['name']);
 
 
-    $sql = "INSERT INTO `formulario` (`id_form`, `art_revisado`, `link`) VALUES (NULL, '$revisado', '$formulario'); ";
+$tmp_file_b = $_FILES['file_b']['tmp_name'];
+$formulario = date("d-m-y-H-i-s") . "-" . normalizar($_FILES['file_b']['name']);
 
-    if ($conex->query($sql) === TRUE) {
-        
-        move_uploaded_file($tmp_file_a, "../../revisiones/documentos/".$revisado);
-        move_uploaded_file($tmp_file_b, "../../formulario/documentos/".$formulario);
-       
+$date = date("Y:m:d H:i:s");
 
-        $sql2 = "SELECT id_form FROM formulario WHERE art_revisado = '$revisado' ";
-        $resultado = $conex->query($sql2);
-        if($resultado->num_rows > 0){ 
-            while($row = $resultado->fetch_assoc()) {
-               
-                $sql = " ";
+$est_aceptado = $_POST["aprobado"];
+$id_art = $_POST["id_art"];
+
+
+$sql = "INSERT INTO `formulario` (`id_form`, `art_revisado`, `link`) VALUES (NULL, '$revisado', '$formulario'); ";
+
+if ($conex->query($sql) === TRUE) {
+
+    move_uploaded_file($tmp_file_a, "../../revisiones/documentos/" . $revisado);
+    move_uploaded_file($tmp_file_b, "../../formulario/documentos/" . $formulario);
+
+
+    $sql2 = "SELECT id_form FROM formulario WHERE art_revisado = '$revisado' ";
+    $resultado = $conex->query($sql2);
+    if ($resultado->num_rows > 0) {
+        while ($row = $resultado->fetch_assoc()) {
+            $id_form = $row["id_form"];
+            $sql3 = "UPDATE `revisar` SET `fecha_revision` = '$date', `est_aceptado` = '$est_aceptado' WHERE `id_form` = '$id_form'";
+            if($conex->query($sql3)) {
 
             }
+            else {
+                $valor = 0;
+            }
         }
-        $valor= 1;
-        $arr = array('valor' => $valor);
-        
-
-
-    }else{
-        $valor= 0;
-        $arr = array('valor' => $valor);
     }
+    $valor = 1;
+    $arr = array('valor' => $valor);
 
+
+
+} else {
+    $valor = 0;
+    $arr = array('valor' => $valor);
 }
+
 
 ?>
